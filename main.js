62d66b77f0a8abcb8e18ee2d10d6933d14ee01c2
DR4G00ON <ramonbrito698@gmail.com>
2024-10-06 13:03:22 -0300

    Starting

---
+++ main.js
@@ -0,0 +1,8 @@
+console.log('Welcome to be xadrez in cmd');
+console.log('----------------------------------------');
+console.log('All the players go have one played for to did');
+
+console.log("Let's go!!!!");
+console.log('========================================');
+
+
